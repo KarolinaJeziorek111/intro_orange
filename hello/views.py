40cb666231181c02a64f5906c890b02e3e9dc9d8
Karolina Jeziorek <karolinajeziorek1@gmail.com>
2023-04-24 19:36:49 +0200

    add and modifiec

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,29 @@
+from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def hello(request):
-    return "Hello, world"
+    return HttpResponse("Hello, world")
+
+def name(request):
+    return HttpResponse("<h2>Witaj, <span style='color:red;'>Karolina</span><h2>")
+
+def hi(request):
+    return HttpResponse(
+        """"
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <meta charset='UTF-8'>
+    </head>
+    <title>Witaj</title>
+    <body>
+    Hi world
+    </body>
+    </html>
+    """
+    )
+
+def hi2(request):
+    return render(request, 'hi_html.html')
--- intro/urls.py
+++ intro/urls.py
@@ -15,12 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 from hello import views
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.hello),
+    path('name/', views.name),
+    path('hi/', views.hi),
+    path('hi2/', views.hi2),
+
+    path('next/', include('next_app.urls')),
 ]
